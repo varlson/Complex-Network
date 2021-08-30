66778d66cd7531a4f0e0a49e99f490388d5f2b1f
varlson <suleimane.ducure@aluno.ufop.edu.br>
2021-08-29 22:50:43 -0300

    random

--- anac.py
+++ anac.py
@@ -5,19 +5,19 @@ from randonCorres import*
 
 def main(g, folder):
 
-    randon_main(g.copy(), folder+'/randon')    
-    metrics  = extractor(g, 1)
-    graphBuilder(g, metrics[1], metrics[0], 'GiantComponent', folder, 1)
+    randon_main(g.copy(), folder+'/Terresrandon')    
+    # metrics  = extractor(g, 1)
+    # graphBuilder(g, metrics[1], metrics[0], 'GiantComponent', folder, 1)
 
-    metrics  = extractor(g, 2)
-    graphBuilder(g, metrics[1], metrics[0], 'Effienciy', folder, 2)
+    # metrics  = extractor(g, 2)
+    # graphBuilder(g, metrics[1], metrics[0], 'Effienciy', folder, 2)
 
-    metrics  = extractor(g, 3)
-    graphBuilder(g, metrics[1], metrics[0], 'TotalFlow', folder, 3)
+    # metrics  = extractor(g, 3)
+    # graphBuilder(g, metrics[1], metrics[0], 'TotalFlow', folder, 3)
 
 
-# g = Graph.Read_GraphML('rmsp.GraphML')
+g = Graph.Read_GraphML('anac/terrestrial.GraphML')
 
-from netGenerator import _main
-g = _main('pas2010')
+# from netGenerator import _main
+# g = _main('terr')
 main(g, 'ibge')
